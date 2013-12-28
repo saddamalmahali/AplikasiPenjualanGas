1fce9fe6f2c7e25a02006fc3fd646943234608d6
saddam <saddam@localhost>
2013-12-28 07:16:13 -0500

    Ineternal Pembelian : penyempurnaan

--- src/com/adam/view/DialogDetailPembelian.java
+++ src/com/adam/view/DialogDetailPembelian.java
@@ -195,7 +195,7 @@ public class DialogDetailPembelian extends javax.swing.JDialog {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 24, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(btnTutup)
                 .addContainerGap())
         );
